ae344d3d6622a81038d3ab3fee9aac4dd746c303
blamp23 <140281033+blamp23@users.noreply.github.com>
2024-03-26 12:03:24 -0500

    Import JSON

--- Code/GO Rev Search.R
+++ Code/GO Rev Search.R
@@ -1,23 +1,43 @@
+library(jsonlite)
+# Import From JSON #############################################################
+url <- "https://maayanlab.cloud/Harmonizome/api/1.0/gene_set/MYC-19030024-MESC-mouse/CHEA+Transcription+Factor+Binding+Site+Profiles"
+data <- fromJSON(url)
+str(data) # Structure may change based on import site, adjust GOI path as needed
+GOI <- data$associations$gene$symbol
+GOI <- mapIds(org.Bt.eg.db,
+              keys = GOI,
+              column = "ENSEMBL",
+              keytype = "SYMBOL",
+              multivals = 'first')
 
-# MYC MAX complex
-file_path <- "C:/Users/12142/Downloads/QuickGO-annotations-1711462600951-20240326.tsv"
+# Import From TSV ##############################################################
+# Taxon ID. 9913, If you want to filter based on BosTau
+# rRNA metabolic process
+file_path <- "C:/Users/12142/Downloads/QuickGO-annotations-1711470871009-20240326.tsv"
 chrom_genes <- read.delim(file_path, sep = "\t")
 head(chrom_genes)
-
 chrom_genes$ensembl <- mapIds(org.Bt.eg.db,
-                       keys = chrom_genes$SYMBOL,
+                       keys = chrom_genes$SYMBOL, # Adjust keys for Symbol
                        column = "ENSEMBL",
                        keytype = "SYMBOL",
                        multivals = 'first')
-gene_of_interest <- c(chrom_genes$ensembl)
+GOI <- c(chrom_genes$ensembl)
+
+# Filter for GOI ###############################################################
 filtered_motif_index <- lapply(motif_index, function(genes) {
-  genes[genes %in% genes_of_interest]
+  genes[genes %in% GOI]
 })
 
 smi <- summary(motif_index)
 sfmi <- summary(filtered_motif_index)
 
+smi
+sfmi
+
+
+# Proportionality Table ########################################################
 # Convert to data frames and convert 'Length' to numeric
+# Length rep # of genes
 smi_df <- data.frame(Motif = rownames(smi), Length = as.numeric(smi[, "Length"]))
 sfmi_df <- data.frame(Motif = rownames(sfmi), Length = as.numeric(sfmi[, "Length"]))
 
@@ -25,16 +45,17 @@ sfmi_df <- data.frame(Motif = rownames(sfmi), Length = as.numeric(sfmi[, "Length
 smi_df <- smi_df[order(smi_df$Motif),]
 sfmi_df <- sfmi_df[order(sfmi_df$Motif),]
 
-#Calculate proportions
+# Calculate proportions
 proportions <-sfmi_df$Length / smi_df$Length
 
 # Combine results
 final_df <- data.frame(Motif = smi_df$Motif, Proportion = proportions)
-final_df$Proportion <- final_df$Proportion * 100
+final_df$Proportion <- final_df$Proportion * 100 # Proportion is a % of 100
 final_df <- final_df[order(-final_df$Proportion),]
 print(final_df)
 
-filtered_motif_index[['SSIS']]
+filtered_motif_index[['SSDI']]
+
 
 
 
