d42117393d13914e72071954189140f539e0bd8d
blamp23 <140281033+blamp23@users.noreply.github.com>
2024-08-27 14:17:34 -0500

    Commit for robustivity

--- miRNA Code/metadata_rawdata.R
+++ miRNA Code/metadata_rawdata.R
@@ -2,7 +2,7 @@ library(readr)
 library(DESeq2)
 
 # Read the CSV file
-file_path <- 'https://raw.githubusercontent.com/blamp23/Cattle-lac-proj/main/Raw_data/Filtered_Raw_Counts_fix.csv'
+file_path <- 'https://raw.githubusercontent.com/blamp23/transcriptional_dynamics_of_bov_mam/main/miRNA/raw_data/mirna_raw_counts.csv'
 df <- read.csv(file_path)
 row.names(df) <- df$X
 df <- df[-1]
@@ -25,4 +25,5 @@ dds <- DESeqDataSetFromMatrix(
 dds <- DESeq(dds)
 
 normalized_counts <- counts(dds, normalized = TRUE)
+
 
